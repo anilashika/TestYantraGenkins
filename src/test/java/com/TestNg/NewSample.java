397d7fc9901e7c28f9ea3f3f2650317c7b8df738
Anitha <Anitha@LAPTOP-UM4DSUAN>
2024-03-06 13:23:55 +0530

    Added test script

---
+++ src/test/java/com/TestNg/NewSample.java
@@ -0,0 +1,17 @@
+package com.TestNg;
+
+import org.testng.annotations.Test;
+
+public class NewSample {
+	@Test(groups="regressionsuite")
+	public void TestScript_7()
+	{
+		System.out.println("TestScript_7");
+	}
+	@Test(groups="smokesuite")
+	public void TestScript_8()
+	{
+		System.out.println("TestScript_8");
+	}
+
+}
