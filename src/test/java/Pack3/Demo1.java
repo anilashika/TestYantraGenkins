42a9fe6152e1ca98f573f8e0f79341bcf40a5d4d
anilashika <anilashika@gmail.com>
2024-03-08 10:00:03 +0530

    testscript ready

---
+++ src/test/java/Pack3/Demo1.java
@@ -0,0 +1,21 @@
+package Pack3;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	@Test(groups="regressionsuite")
+	public void TestScript_5()
+	{
+		System.out.println("TestScript_9");
+	}
+	@Test(groups="smokesuite")
+	public void TestScript_6()
+	{
+		System.out.println("TestScript_10");
+	}
+
+
+
+
+
+}
